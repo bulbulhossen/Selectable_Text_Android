839686a4ddaae0f4c6b1e450883da94b05914e7f
Bulbul Hossen <Bulbul Hossen>
2016-03-17 23:37:05 +0600

    Initial commit

---
+++ app/src/main/java/com/bulbulhossen/selectabletext/OnScrollChangedListener.java
@@ -0,0 +1,9 @@
+package com.bulbulhossen.selectabletext;
+
+/**
+ * Created by appsd on 17-Mar-16.
+ */
+
+public interface OnScrollChangedListener {
+    public void onScrollChanged(ObservableScrollView scrollView, int x, int y, int oldx, int oldy);
+}
